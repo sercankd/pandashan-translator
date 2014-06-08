d6f42d66f48049be27cf8d0090485ca512e434eb
sercankd <webbilgi@gmail.com>
2014-06-08 13:21:14 +0300

    initial release

---
+++ Pandashan Item Translator/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RecuperareIIButton1 = New Pandashan_Item_Translator.RecuperareIIButton()
+        Me.RecuperareIITextBox2 = New Pandashan_Item_Translator.RecuperareIITextBox()
+        Me.RecuperareIITextBox1 = New Pandashan_Item_Translator.RecuperareIITextBox()
+        Me.RecuperareIIStatusBar1 = New Pandashan_Item_Translator.RecuperareIIStatusBar()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 39)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(315, 173)
+        Me.ListBox1.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'RecuperareIIButton1
+        '
+        Me.RecuperareIIButton1.BackColor = System.Drawing.Color.Transparent
+        Me.RecuperareIIButton1.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Bold)
+        Me.RecuperareIIButton1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.RecuperareIIButton1.Location = New System.Drawing.Point(292, 218)
+        Me.RecuperareIIButton1.Name = "RecuperareIIButton1"
+        Me.RecuperareIIButton1.Size = New System.Drawing.Size(33, 21)
+        Me.RecuperareIIButton1.TabIndex = 8
+        Me.RecuperareIIButton1.Text = "Copy"
+        '
+        'RecuperareIITextBox2
+        '
+        Me.RecuperareIITextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer))
+        Me.RecuperareIITextBox2.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Bold)
+        Me.RecuperareIITextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.RecuperareIITextBox2.Location = New System.Drawing.Point(12, 13)
+        Me.RecuperareIITextBox2.MaxLength = 32767
+        Me.RecuperareIITextBox2.MultiLine = False
+        Me.RecuperareIITextBox2.Name = "RecuperareIITextBox2"
+        Me.RecuperareIITextBox2.Size = New System.Drawing.Size(315, 21)
+        Me.RecuperareIITextBox2.TabIndex = 7
+        Me.RecuperareIITextBox2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.RecuperareIITextBox2.UseSystemPasswordChar = False
+        '
+        'RecuperareIITextBox1
+        '
+        Me.RecuperareIITextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer), CType(CType(233, Byte), Integer))
+        Me.RecuperareIITextBox1.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Bold)
+        Me.RecuperareIITextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.RecuperareIITextBox1.Location = New System.Drawing.Point(12, 218)
+        Me.RecuperareIITextBox1.MaxLength = 32767
+        Me.RecuperareIITextBox1.MultiLine = False
+        Me.RecuperareIITextBox1.Name = "RecuperareIITextBox1"
+        Me.RecuperareIITextBox1.Size = New System.Drawing.Size(274, 21)
+        Me.RecuperareIITextBox1.TabIndex = 6
+        Me.RecuperareIITextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.RecuperareIITextBox1.UseSystemPasswordChar = False
+        '
+        'RecuperareIIStatusBar1
+        '
+        Me.RecuperareIIStatusBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.RecuperareIIStatusBar1.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Bold)
+        Me.RecuperareIIStatusBar1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.RecuperareIIStatusBar1.Location = New System.Drawing.Point(0, 250)
+        Me.RecuperareIIStatusBar1.Name = "RecuperareIIStatusBar1"
+        Me.RecuperareIIStatusBar1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.RecuperareIIStatusBar1.Size = New System.Drawing.Size(337, 23)
+        Me.RecuperareIIStatusBar1.TabIndex = 5
+        Me.RecuperareIIStatusBar1.Text = "by Sercankd"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(337, 273)
+        Me.Controls.Add(Me.RecuperareIIButton1)
+        Me.Controls.Add(Me.RecuperareIITextBox2)
+        Me.Controls.Add(Me.RecuperareIITextBox1)
+        Me.Controls.Add(Me.RecuperareIIStatusBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Pandashan Item Name Translator"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents RecuperareIIStatusBar1 As Pandashan_Item_Translator.RecuperareIIStatusBar
+    Friend WithEvents RecuperareIITextBox1 As Pandashan_Item_Translator.RecuperareIITextBox
+    Friend WithEvents RecuperareIITextBox2 As Pandashan_Item_Translator.RecuperareIITextBox
+    Friend WithEvents RecuperareIIButton1 As Pandashan_Item_Translator.RecuperareIIButton
+
+End Class
---
+++ Pandashan Item Translator/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Data
+Imports System.Data.SQLite
+Imports System.Threading.Thread
+Public Class Form1
+    Dim cons As New SQLite.SQLiteConnection
+    Dim cmd As New SQLite.SQLiteCommand
+    Dim sql As String
+    Dim strSQL As String
+    Dim dss As New DataSet
+    Dim das As New SQLite.SQLiteDataAdapter
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cons.ConnectionString = "Data Source=database.db; Version=3"
+        cons.Open()
+    End Sub
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        RecuperareIITextBox2.Select()
+    End Sub
+    Private Sub RecuperareIITextBox2_TextChanged(sender As Object, e As EventArgs) Handles RecuperareIITextBox2.TextChanged
+        If RecuperareIITextBox2.Text.Length > 3 Then
+            cmd = cons.CreateCommand
+            cmd.CommandText = "SELECT * FROM items WHERE string_en like ""%" & RecuperareIITextBox2.Text & "%"""
+            Dim SQLreader As SQLiteDataReader = cmd.ExecuteReader()
+            ListBox1.Items.Clear()
+            While SQLreader.Read()
+                Sleep(3000)
+                ListBox1.Items.Add(String.Format("{1}", SQLreader(0), SQLreader(1), SQLreader(2)))
+            End While
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Dim SelectedItem = ListBox1.SelectedItem.ToString
+        cmd = cons.CreateCommand
+        cmd.CommandText = "SELECT * FROM items WHERE string_en = """ & SelectedItem & """"
+        Dim SQLreader As SQLiteDataReader = cmd.ExecuteReader()
+        While SQLreader.Read()
+
+            RecuperareIITextBox1.Text = (String.Format("{2}", SQLreader(0), SQLreader(1), SQLreader(2)))
+        End While
+    End Sub
+
+    Private Sub RecuperareIIButton1_Click(sender As Object, e As EventArgs) Handles RecuperareIIButton1.Click
+        Clipboard.SetText(RecuperareIITextBox1.Text)
+    End Sub
+End Class
